320aaee2eebca461ca6eb11a98df72e434eea952
Stuart Hendren <stuart@committed.io>
2020-12-23 10:46:38 +0000

    Add tests for GraphToolbar

--- src/components/Graph/Graph.test.tsx
+++ src/components/Graph/Graph.test.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { render } from '../../setupTests'
 import { CustomRenderer } from './Graph.stories'
 
+// TODO Add canvas to jest:
+// Error: Not implemented: HTMLCanvasElement.prototype.getContext (without installing the canvas npm package)
 it('renders story', () => {
   render(<CustomRenderer />)
 })
--- src/components/GraphToolbar/GraphToolbar.test.tsx
+++ src/components/GraphToolbar/GraphToolbar.test.tsx
@@ -1,6 +1,59 @@
-it('renders story', () => {
-  // TODO Add canvas to jest:
-  // Error: Not implemented: HTMLCanvasElement.prototype.getContext (without installing the canvas npm package)
-  // const { asFragment } = render(<Horizontal />)
-  // expect(asFragment()).toMatchSnapshot()
+import { Flex } from '@committed/components'
+import React, { useState } from 'react'
+import { GraphToolbar } from '.'
+import { addRandomEdge, addRandomNode } from '../../graph/data/Generator'
+import { GraphModel } from '../../graph/GraphModel'
+import { renderDark, renderLight, userEvent } from '../../setupTests'
+
+const Template: React.FC<{ direction: 'row' | 'column' }> = ({ direction }) => {
+  const [model, setModel] = useState(
+    addRandomEdge(addRandomNode(GraphModel.createEmpty(), 20), 15)
+  )
+  return (
+    <Flex flexDirection={direction === 'row' ? 'column' : 'row'}>
+      <GraphToolbar
+        direction={direction}
+        model={model}
+        onModelChange={setModel}
+      />
+    </Flex>
+  )
+}
+
+export const Vertical: React.FC = () => {
+  return <Template direction="column" />
+}
+
+export const Horizontal: React.FC = () => {
+  return <Template direction="row" />
+}
+
+it('renders light', () => {
+  const { asFragment } = renderLight(<Horizontal />)
+  expect(asFragment()).toMatchSnapshot()
+})
+
+it('renders dark', () => {
+  const { asFragment } = renderDark(<Horizontal />)
+  expect(asFragment()).toMatchSnapshot()
+})
+
+test.each([
+  [/zoom-in/i, 'ZoomIn'],
+  [/zoom-out/i, 'ZoomOut'],
+  [/layout/i, 'Layout'],
+  [/refit/i, 'Refit'],
+])('button label %s issues command %s', (pattern, command) => {
+  const onChange = jest.fn()
+  const { getByRole } = renderLight(
+    <GraphToolbar
+      direction="row"
+      model={GraphModel.createEmpty()}
+      onModelChange={onChange}
+    />
+  )
+
+  userEvent.click(getByRole('button', { name: pattern }))
+  const newModel = onChange.mock.calls[0][0] as GraphModel
+  expect(newModel.getCommands()[0]).toEqual({ type: command })
 })
