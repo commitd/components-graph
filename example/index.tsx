0bdd534865bbec54618c90f5b97d40f978094497
Stuart Hendren <stuart@committed.io>
2020-12-30 18:19:34 +0000

    Improve example

--- example/index.tsx
+++ example/index.tsx
@@ -1,14 +1,22 @@
+import {
+  AppBar,
+  Box,
+  Heading,
+  Row,
+  ThemeProvider,
+  ThemeSwitch,
+  Toolbar,
+} from '@committed/components'
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import {
   addRandomEdge,
   addRandomNode,
+  cytoscapeRenderer,
   Graph,
   GraphModel,
   GraphToolbar,
-  cytoscapeRenderer,
 } from '../dist'
-import { Row, ThemeProvider } from '@committed/components'
 
 const App: React.FC = () => {
   const [model, setModel] = React.useState(
@@ -16,12 +24,21 @@ const App: React.FC = () => {
   )
   return (
     <ThemeProvider>
+      <AppBar position="relative">
+        <Toolbar>
+          <Box flexGrow={1}>
+            <Heading.h1>Components Graph</Heading.h1>
+          </Box>
+          <GraphToolbar
+            direction="row"
+            model={model}
+            onModelChange={setModel}
+            color="inherit"
+          />
+          <ThemeSwitch />
+        </Toolbar>
+      </AppBar>
       <Row>
-        <GraphToolbar
-          direction="column"
-          model={model}
-          onModelChange={setModel}
-        />
         <Graph
           model={model}
           onModelChange={setModel}
