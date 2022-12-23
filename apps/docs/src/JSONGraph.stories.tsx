082496c95d6e99cc60c03d4591257d17bfe1e953
Stuart Hendren <stuart@committed.io>
2022-12-23 10:44:45 +0000

    docs: adds json graph visualizer to the stories
    uses relaxed-json to parse the input to make it simpler

--- apps/docs/src/JSONGraph.stories.tsx
+++ apps/docs/src/JSONGraph.stories.tsx
@@ -1,8 +1,10 @@
-import { Graph, GraphModel } from '@committed/components-graph'
+import { Alert, AlertContent, AlertTitle, Flex } from '@committed/components'
+import { ContentModel, cytoscapeRenderer, Graph, GraphModel, GraphToolbar, ModelNode, NodeViewer } from '@committed/components-graph'
 import { Json, JsonExample } from '@committed/graph-json'
-import { Meta } from '@storybook/react'
-import React, { useState } from 'react'
+import { Meta, Story } from '@storybook/react'
+import React, { useEffect, useState } from 'react'
 import { Template } from './StoryUtil'
+import RJSON from "relaxed-json"
 
 const { smallGraph, largeGraph } = JsonExample
 
@@ -29,4 +31,101 @@ export const RichJsonGraph: React.FC = () => {
     GraphModel.createWithContent(Json.buildGraph(largeGraph))
   )
   return <Template model={model} onModelChange={setModel} />
+}
+
+const StoryTemplate: Story<{
+  json: string
+}> = ({
+  json,
+}) => {
+  const [model, setModel] = useState(
+    GraphModel.createWithContent(ContentModel.createEmpty())
+  )
+  const [alert, setAlert] = useState<string | undefined>()
+
+  useEffect(() => {
+    try {
+      setModel(GraphModel.createWithContent(Json.buildGraph(RJSON.parse(json))))
+      setAlert(undefined)
+    } catch (error: any) {
+      setModel(GraphModel.createWithContent(ContentModel.createEmpty()))
+      setAlert(error.message)
+    }
+  }, [setModel, setAlert, json])
+
+  const [node, setNode] = useState<ModelNode | undefined>(undefined)
+
+  return (
+    <>
+      {alert && (
+        <Alert severity="error">
+          <AlertTitle>JSON Error</AlertTitle>
+          <AlertContent>{alert}</AlertContent>
+        </Alert>
+      )}
+      <Flex>
+        <GraphToolbar
+          direction="column"
+          model={model}
+          onModelChange={setModel}
+          layouts={cytoscapeRenderer.layouts}
+        />
+        <Graph
+          model={model}
+          onModelChange={setModel}
+          renderer={cytoscapeRenderer}
+          options={{ height: 600 }}
+          onViewNode={setNode}
+        />
+        <NodeViewer
+          open={node != undefined}
+          node={node}
+          onOpenChange={() => setNode(undefined)}
+        />
+      </Flex>
+    </>
+  )
+}
+
+export const JsonVisualizer = StoryTemplate.bind({})
+JsonVisualizer.args = {
+  json: `
+/* 
+ * We parse this with relaxed-json to make entering the json a little easier
+ * http://oleg.fi/relaxed-json/
+ */
+{
+  graph: {
+    id: "car-manufacturer-relationships",
+    type: "car",
+    label: "Car Manufacturer Relationships",
+    nodes: {
+      nissan: {
+        label: "Nissan",
+      },
+      infiniti: {
+        label: "Infiniti",
+      },
+      toyota: {
+        label: "Toyota",
+      },
+      lexus: {
+        label: "Lexus",
+      },
+    },
+    edges: [
+      {
+        source: "nissan",
+        target: "infiniti",
+        relation: "has_luxury_division",
+      },
+      {
+        source: "toyota",
+        target: "lexus",
+        relation: "has_luxury_division",
+      },
+    ],
+  },
+} 
+`,
 }
